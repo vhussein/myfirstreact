5cbfb2b99b9c9d6d60ddac00d261c12f4435bfd8
Azlan <vhussein80@gmail.com>
2019-11-09 15:10:17 +0800

    updated gitignore

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is my react bros
         </a>
       </header>
     </div>
